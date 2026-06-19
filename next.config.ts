6c3aa2480dff6de8f6d319933305bbc372bdd3cc
robekenn <robekenn@oregonstate.edu>
2026-06-18 19:34:41 -0700

    Relax CSP for Next production scripts

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self'",
-              "script-src 'self'",
+              "script-src 'self' 'unsafe-inline'",
               "style-src 'self' 'unsafe-inline'",
               "img-src 'self' blob: data:",
               "font-src 'self'",
